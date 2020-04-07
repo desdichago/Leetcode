7c22610c086fcd8c96d5c753e03c8b4c9f0991c1
Alex F <desdichago@gmail.com>
2020-04-07 07:53:12 +0300

    P1TwoSumEasy

---
+++ src/test/java/P1TwoSumEasy/SolutionTest.java
@@ -0,0 +1,18 @@
+package P1TwoSumEasy;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void twoSum() {
+        int[] input = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] expected = new int[]{0, 1};
+        Solution solution = new Solution();
+        int[] actual = solution.twoSum(input, target);
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
